756a98b68be630bb43f9f30a5891335c52befae5
b3nj4m1n <b3nj4m1n@gmx.net>
2020-05-01 02:03:48 +0200

    Moved yay installation

--- update.sh
+++ update.sh
@@ -22,6 +22,15 @@ mkdir -p ~/Documents/Github
 # Cd into github dir
 cd ~/Documents/Github
 
+# Only do this if argument --yay is given
+if ! hash yay 2>/dev/null; then
+    mkdir -p ~/Documents/Github
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    mkdir -p ~/Documents/Github
+fi
+
 # Only do this if argument -p is given
 match="-p"
 if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then    
@@ -55,13 +64,6 @@ if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
         pacman -S --noconfirm --needed makepkg
         pacman -S --noconfirm --needed curl
         pacman -S --noconfirm --needed wget
-        if ! hash yay 2>/dev/null; then
-            mkdir -p ~/Documents/Github
-            git clone https://aur.archlinux.org/yay.git
-            cd yay
-            makepkg -si
-            mkdir -p ~/Documents/Github
-        fi
         pacman -S --noconfirm --needed python
         pacman -S --noconfirm --needed python2
         pacman -S --noconfirm --needed make
