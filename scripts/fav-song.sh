0ba6ddbf290c86eb5f386406415172a7d4f08ac7
b3nj4m1n <b3nj4m1n@gmx.net>
2020-05-16 23:10:11 +0200

    Added more playlists to auto add script

--- scripts/fav-song.sh
+++ scripts/fav-song.sh
@@ -3,8 +3,22 @@
 # This script grabs the track currently played by cmus and puts the filename into a playlist file
 # To reload all playlists an external script needs to be used at a later time (Cmus needs to be closed)
 
-# Playlist to which the song will be written
+# Get args in array
+args=("$@")
+
+# Playlist to which the song will be written if no args are given
 filename="/mnt/Vault/music/favs.m3u"
+
+# Chill playlist
+match="--chill"
+if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
+    filename="/mnt/Vault/music/chill.m3u"
+fi
+# Metal playlist
+match="--metal"
+if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
+    filename="/mnt/Vault/music/metal.m3u"
+fi
 
 # Get relative path to song and write it to the file
 cmus-remote -Q 2>/dev/null | sed "s/file \/mnt\/Vault\/music/./;t;d" >> $filename
