3a67f964cc5522afebdfa5467f6402aabdb9dbc5
b3nj4m1n <b3nj4m1n@gmx.net>
2020-05-28 18:03:58 +0200

    Changed song fav script

--- scripts/fav-song.sh
+++ scripts/fav-song.sh
@@ -3,22 +3,31 @@
 # This script grabs the track currently played by cmus and puts the filename into a playlist file
 # To reload all playlists an external script needs to be used at a later time (Cmus needs to be closed)
 
-# Get args in array
-args=("$@")
+# Directory to search for m3u files
+dirname="/mnt/Vault/music"
+# Filter for finding playlist files
+filter="a.*.m3u"
 
-# Playlist to which the song will be written if no args are given
-filename="/mnt/Vault/music/a.favs.m3u"
+# # Get args in array
+# args=("$@")
 
-# Chill playlist
-match="--chill"
-if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
-    filename="/mnt/Vault/music/a.chill.m3u"
-fi
-# Metal playlist
-match="--metal"
-if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
-    filename="/mnt/Vault/music/a.metal.m3u"
-fi
+# # Playlist to which the song will be written if no args are given
+# filename="/mnt/Vault/music/a.favs.m3u"
+
+# # Chill playlist
+# match="--chill"
+# if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
+#     filename="/mnt/Vault/music/a.chill.m3u"
+# fi
+# # Metal playlist
+# match="--metal"
+# if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
+#     filename="/mnt/Vault/music/a.metal.m3u"
+# fi
+
+# Get selection
+files=$(find $dirname -name $filter)
+filename=$(printf "$files" | dmenu -c)
 
 # Get relative path to song and write it to the file
 cmus-remote -Q 2>/dev/null | sed "s/file \/mnt\/Vault\/music/./;t;d" >> $filename
