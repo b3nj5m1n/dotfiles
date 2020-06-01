550cc1ab4c2032a4d6a420f5788fe4c866d3fd57
b3nj4m1n <b3nj4m1n@gmx.net>
2020-06-01 13:23:23 +0200

    Added some programms to autostart

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
+
+# Daemons & stuff
 ~/wally.sh -r -t nature
 picom &
 dunst &
 sxhkd &
 dwmblocks &
+
+# Programms
+firefox --class="init_firefox" &
+alacritty --class "cmus" -e "cmus" &
+discord &
+anki &
+thunderbird --class "init_thunderbird" &
+# keeweb &
+syncthing
+
 # ~/.config/polybar/polybar.sh
