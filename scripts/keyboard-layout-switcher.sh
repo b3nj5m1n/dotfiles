a72ddc9b80c19dfc4ff5048a91ebd6a65868a923
b3nj4m1n <b3nj4m1n@gmx.net>
2020-06-07 18:08:17 +0200

    Rework keyboard-layout-switcher.sh

--- scripts/keyboard-layout-switcher.sh
+++ scripts/keyboard-layout-switcher.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-selection=$(printf "german\ndvorak\nprogrammer dvorak\nenglish" | dmenu -c)
+german="german"
+dvorak="dvorak"
+pdvorak="programmer dvorak"
+english="english"
+
+selection=$(printf "$german\n$dvorak\n$pdvorak\n$english" | dmenu -c)
 
 # Remove all custom mappings
 setxkbmap -option
 
-if [[ "$selection" = "german" ]]; then
+if [[ "$selection" = "$german" ]]; then
     setxkbmap de
-elif [[ "$selection" = "dvorak" ]]; then
+elif [[ "$selection" = "$dvorak" ]]; then
     setxkbmap gb dvorak
-elif [[ "$selection" = "programmer dvorak" ]]; then
+elif [[ "$selection" = "$pdvorak" ]]; then
     setxkbmap -layout us -variant dvp -option compose:102 -option numpad:shift3 -option kpdl:semi -option keypad:atm -option caps:shift
     xmodmap -e "keycode 94=0x007c"
-elif [[ "$selection" = "english" ]]; then
+elif [[ "$selection" = "$english" ]]; then
     setxkbmap gb
 fi
 
-pkill -RTMIN+2 dwmblocks
+# Send notification
+notify-send -a "keyboard-layout-switcher" "Layout Switcher" "Switched keyboard layout to <i>$selection</i>"
