b39e402f17bfabeb6c62f1ebe694d0a605205d3a
b3nj4m1n <b3nj4m1n@gmx.net>
2020-06-07 17:49:20 +0200

    Rework fav-song.sh

--- scripts/fav-song.sh
+++ scripts/fav-song.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
 # This script grabs the track currently played by cmus and puts the filename into a playlist file
-# To reload all playlists an external script needs to be used at a later time (Cmus needs to be closed)
+# The menu displayed will contain every file in the specified dir starting with a.
+# Typing any name will automatically save to a new filename (Full path needs to be typed)
 
 # Directory to search for m3u files
 dirname="/mnt/Vault/music"
 # Filter for finding playlist files
 filter="a.*.m3u"
 
-# # Get args in array
-# args=("$@")
-
-# # Playlist to which the song will be written if no args are given
-# filename="/mnt/Vault/music/a.favs.m3u"
-
-# # Chill playlist
-# match="--chill"
-# if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
-#     filename="/mnt/Vault/music/a.chill.m3u"
-# fi
-# # Metal playlist
-# match="--metal"
-# if printf '%s\n' ${args[@]} | grep -q -P '^'$match'$'; then
-#     filename="/mnt/Vault/music/a.metal.m3u"
-# fi
-
 # Get selection
-files=$(find $dirname -name $filter -maxdepth 1)
+files=$(find $dirname -maxdepth 1 -name $filter)
+# File to write to
 filename=$(printf "$files" | dmenu -c)
 
 # Get relative path to song and write it to the file
-cmus-remote -Q 2>/dev/null | sed "s/file \/mnt\/Vault\/music/./;t;d" >> $filename
+songname=$(cmus-remote -Q 2>/dev/null | sed "s/file \/mnt\/Vault\/music/./;t;d")
+printf "%s" "$songname" >> $filename
+
+# Display notification
+filename=$(echo "$filename" | rev | cut -d '/' -f 1 | rev)
+songname=$(echo "$songname" | rev | cut -d '/' -f 1 | rev)
+notify-send -a "fav-song" "Save song" "Song <i>$songname</i> to <i>$filename</i> successfully"
