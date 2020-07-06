d9ff8eef935ba16e789e003627c0ce9ea9c68e51
b3nj4m1n <b3nj4m1n@gmx.net>
2020-07-06 17:38:42 +0200

    lemonblocks script

---
+++ scripts/lemonblocks/network.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+down=$(( $(cat /sys/class/net/enp2s0/statistics/rx_bytes) / 1024**2 ))
+up=$(( $(cat /sys/class/net/enp2s0/statistics/tx_bytes) / 1024**2 ))
+
+printf "[%dMiB %dMiB]" "$down" "$up"
