6c214b10587e8e9454684722f2cf6a3f8f5a1632
b3nj4m1n <b3nj4m1n@gmx.net>
2020-07-07 17:52:31 +0200

    Fixed uptime script

--- scripts/lemonblocks/uptime.sh
+++ scripts/lemonblocks/uptime.sh
@@ -6,7 +6,7 @@ printf "%%{F-}"
 
 FILE=/tmp/verboseuptime
 if [[ -f "$FILE" ]]; then
-    printf "%%{A1:rm /tmp/verboseuptime; pkill lemonblocks -11:}uptime: $(uptime | cut -d ' ' -f 5 | tr ',' ' ')%%{A1}"
+    printf "%%{A1:rm /tmp/verboseuptime; pkill lemonblocks -11:}uptime: $(uptime -p | sed 's/up //g')%%{A1}"
 else
-    printf "%%{A1:touch /tmp/verboseuptime; pkill lemonblocks -11:}$(uptime | cut -d ' ' -f 5 | tr ',' ' ')%%{A1}"
+    printf "%%{A1:touch /tmp/verboseuptime; pkill lemonblocks -11:}$(uptime -p | sed 's/up //g')%%{A1}"
 fi
