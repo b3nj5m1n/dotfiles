20fe8fc35b58d30618c64f92b971bc681d302f52
b3nj4m1n <b3nj4m1n@gmx.net>
2020-11-27 13:22:27 +0100

    Fix case sensitive search for program launcher

--- scripts/program-launcher.sh
+++ scripts/program-launcher.sh
@@ -6,7 +6,7 @@ MAP="/home/b3nj4m1n/.config/rofi/map.csv"
 
 cat "$MAP" \
     | cut -d ',' -f 1 \
-    | rofi -dmenu -p "Util " \
+    | rofi -dmenu -i -p "Util " \
     | head -n 1 \
     | xargs -i --no-run-if-empty grep "{}" "$MAP" \
     | cut -d ',' -f 2 \
