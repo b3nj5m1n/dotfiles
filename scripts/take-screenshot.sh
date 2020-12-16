498a3bbe699a9e58053115d4eb6b3d00d8feb718
b3nj4m1n <b3nj4m1n@gmx.net>
2020-12-16 04:55:29 +0100

    Add option to display screenshot after taking it

--- scripts/take-screenshot.sh
+++ scripts/take-screenshot.sh
@@ -7,5 +7,12 @@ filename=~/Pictures/screenshots/$(date "+%d-%m-%y_%S-%M-%H").png
 
 import -window root -crop $G "$filename"
 
+# Send notification, keep track of whether or not it has been clicked
+ACTION=$(dunstify --action "default,Reply" "Screenshot was saved to <i>$filename</i>")
+# It has been clicked, display the image
+if [ "$ACTION" -eq 2 ]; then
+    xdg-open "$filename"
+fi
+
 # Send notification
-notify-send -a "take-screenshot" "Screenshot" "Screenshot was saved to <i>$filename</i>"
+# notify-send -a "take-screenshot" "Screenshot" "Screenshot was saved to <i>$filename</i>"
