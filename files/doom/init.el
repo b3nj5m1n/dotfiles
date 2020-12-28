1432b609a19d9c187558d90da26fec8030a82e97
b3nj4m1n <b3nj4m1n@gmx.net>
2020-12-28 20:15:18 +0100

    Update doom config

--- files/doom/init.el
+++ files/doom/init.el
@@ -146,7 +146,8 @@
        ; ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +journal +gnuplot +pandoc +present +pretty +dragndrop)         ; organize your plain life in plain text
+       ; (org +roam +journal +gnuplot +pandoc +present +pretty +dragndrop)         ; organize your plain life in plain text
+       (org +journal +gnuplot +pandoc +present +pretty +dragndrop)         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
