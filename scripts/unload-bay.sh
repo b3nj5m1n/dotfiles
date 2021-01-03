6e92dad1daee548a3b0c6521345287fc2af59d8f
b3nj4m1n <b3nj4m1n@gmx.net>
2021-01-03 01:11:56 +0100

    Removed old scripts

--- scripts/unload-bay.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script copys all the files from the bay folder to their destinations on the real filesystem
-
-rsync -rd /home/b3nj4m1n/dotfiles/bay/root/* /
-cp -a /home/b3nj4m1n/dotfiles/bay/root/* /
-cp -r --attributes-only --preserve=mode /home/b3nj4m1n/dotfiles/bay/root/* /
