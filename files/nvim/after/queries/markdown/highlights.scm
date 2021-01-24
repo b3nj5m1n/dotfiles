5cef6f149c48dbaef548874db0ad75c4aa3917eb
b3nj4m1n <b3nj4m1n@gmx.net>
2021-01-24 10:28:30 +0100

    Make highlights work with new treesitter markdown grammar

--- files/nvim/after/queries/markdown/highlights.scm
+++ files/nvim/after/queries/markdown/highlights.scm
@@ -1,23 +1,18 @@
 ((atx_heading
-  (atx_heading_marker) @_h1
-  (_) @h1)
- (#eq? @_h1 "#"))
+  (atx_h1_marker) @_h1
+  (_) @h1))
 ((atx_heading
-  (atx_heading_marker) @_h2
-  (_) @h2)
- (#eq? @_h2 "##"))
+  (atx_h2_marker) @_h2
+  (_) @h2))
 ((atx_heading
-  (atx_heading_marker) @_h3
-  (_) @h3)
- (#eq? @_h3 "###"))
+  (atx_h3_marker) @_h3
+  (_) @h3))
 ((atx_heading
-  (atx_heading_marker) @_h4
-  (_) @h4)
- (#eq? @_h4 "####"))
+  (atx_h4_marker) @_h4
+  (_) @h4))
 ((atx_heading
-  (atx_heading_marker) @_h5
-  (_) @h5)
- (#eq? @_h5 "#####"))
+  (atx_h5_marker) @_h5
+  (_) @h5))
 
 (emphasis) @emphasis
 (strong_emphasis) @strong_emphasis
