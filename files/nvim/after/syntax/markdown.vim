01beca8d8cec599b705ae5d9e5906adcdc9d2c12
b3nj4m1n <b3nj4m1n@gmx.net>
2021-01-26 13:20:21 +0100

    Treesitter markdown parser is too buggy

--- files/nvim/after/syntax/markdown.vim
+++ files/nvim/after/syntax/markdown.vim
@@ -12,3 +12,14 @@ highlight emphasis gui=italic
 highlight strong_emphasis gui=bold
 highlight strikethrough gui=strikethrough
 highlight info_string guifg=#f1fa8c gui=italic
+
+highlight markdownH1 guifg=#50fa7b gui=bold
+highlight markdownH1Delimiter guifg=#50fa7b
+highlight markdownH2 guifg=#ff79c6 gui=bold
+highlight markdownH2Delimiter guifg=#ff79c6
+highlight markdownH3 guifg=#ffb86c gui=bold
+highlight markdownH3Delimiter guifg=#ffb86c
+highlight markdownH4 guifg=#8be9fd gui=bold
+highlight markdownH4Delimiter guifg=#8be9fd
+highlight markdownH5 guifg=#ff5555 gui=bold
+highlight markdownH5Delimiter guifg=#ff5555
--- files/nvim/lua/plugin-config.lua
+++ files/nvim/lua/plugin-config.lua
@@ -64,14 +64,14 @@ require'nvim-treesitter.configs'.setup {
         enable = true
     }
 }
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.markdown = {
-    install_info = {
-        url = "https://github.com/ikatyang/tree-sitter-markdown", -- local path or git repo
-        files = {"src/parser.c", "src/scanner.cc"}
-    },
-    filetype = "md", -- if filetype does not agrees with parser name
-}
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- parser_config.markdown = {
+--     install_info = {
+--         url = "https://github.com/ikatyang/tree-sitter-markdown", -- local path or git repo
+--         files = {"src/parser.c", "src/scanner.cc"}
+--     },
+--     filetype = "md", -- if filetype does not agrees with parser name
+-- }
 
 
 --- colorizer ---
