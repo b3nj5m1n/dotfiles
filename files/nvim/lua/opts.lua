01259bcda3683643c40d84a06617848620669c37
b3nj4m1n <b3nj4m1n@gmx.net>
2021-02-16 16:03:59 +0100

    neovim config & screenshots
    Neovim: Sort options; Hopefully fix cursor shape;
    Add flameshot and make default for taking screenshots;

--- files/nvim/lua/opts.lua
+++ files/nvim/lua/opts.lua
@@ -34,40 +34,41 @@ vim.api.nvim_exec('autocmd ColorScheme * highlight StatusLineNC guifg=#bd93f9 gu
 vim.cmd('colorscheme dracula') -- Use dracula colorscheme
 
 -- Global Settings
-opt_manager.add_option_global("hidden", true) -- Don't kill buffers, when exiting, keep them around in the background
-opt_manager.add_option_global("showmode", false) -- Hide the -- INSERT -- at the bottom
 opt_manager.add_option_global("backup", false) -- Don't create a backup file before overwriting a file
+opt_manager.add_option_global("completeopt", "noinsert,menuone,noselect") -- Do not insert anything until the user selects it; Show the menu when there is just one match; Force the user to select something from the menu
 opt_manager.add_option_global("encoding", "UTF-8") -- Directly jump to next match when searching
 opt_manager.add_option_global("errorbells", false) -- Disable error bell & screen flashing
 opt_manager.add_option_global("foldlevel", 99) -- The higher, the more folded regions are open (0 = all folds closed)
 opt_manager.add_option_global("foldlevelstart", 99) -- Inital fold level; close all folds when opening a new buffer
-opt_manager.add_option_global("ignorecase", true) -- Ignore case in searches
+opt_manager.add_option_global("guicursor", "n-v-c:block-Cursor/lCursor-blinkon0,i-ci:ver25-Cursor/lCursor,r-cr:hor20-Cursor/lCursor") -- Cursor shapes
+opt_manager.add_option_global("hidden", true) -- Don't kill buffers, when exiting, keep them around in the background
 opt_manager.add_option_global("hlsearch", false) -- No highlighting on search
+opt_manager.add_option_global("ignorecase", true) -- Ignore case in searches
 opt_manager.add_option_global("inccommand", "split") -- Show preview of substitute
 opt_manager.add_option_global("incsearch", true) -- Directly jump to next match when searching
 opt_manager.add_option_global("mouse", 'a') -- Enable mouse support
+opt_manager.add_option_global("shortmess", vim.api.nvim_get_option("shortmess") .. "c") -- Avoid prompts for auto-complete
+opt_manager.add_option_global("showmode", false) -- Hide the -- INSERT -- at the bottom
 opt_manager.add_option_global("smartcase", true) -- Don't ignore case in searches when using case in search
 opt_manager.add_option_global("swapfile", false) -- Don't create swapfiles
 opt_manager.add_option_global("undodir", vim.fn.expand("~/.local/share/nvim/undodir")) -- File to store undo stacks in
-opt_manager.add_option_global("completeopt", "noinsert,menuone,noselect") -- Do not insert anything until the user selects it; Show the menu when there is just one match; Force the user to select something from the menu
-opt_manager.add_option_global("shortmess", vim.api.nvim_get_option("shortmess") .. "c") -- Avoid prompts for auto-complete
 
 -- Window Settings
 opt_manager.add_option_window(0, "cursorcolumn", false) -- Indicate current column
 opt_manager.add_option_window(0, "cursorline", true) -- Indicate current line
-opt_manager.add_option_window(0, "foldmethod", "expr") -- Auto fold method set to syntax (Determine folds based on file specific syntax)
 opt_manager.add_option_window(0, "foldexpr", "nvim_treesitter#foldexpr()") -- Auto fold based on treesitter
+opt_manager.add_option_window(0, "foldmethod", "expr") -- Auto fold method set to syntax (Determine folds based on file specific syntax)
 opt_manager.add_option_window(0, "foldnestmax", 20) -- Deepest possible fold
 opt_manager.add_option_window(0, "wrap", false) -- Long lines are displayed as one line (Horizontal scrolling required)
 opt_manager.add_option_window(0, 'number', true) -- Enable line numbers
 opt_manager.add_option_window(0, 'relativenumber', true) -- Enable relative line numbers
 
 -- Buffer Setttings
-opt_manager.add_option_buffer(0, "spelllang", "en_GB,de,es,cjk") -- Dictionarys to use for checking spelling
 opt_manager.add_option_buffer(0, "expandtab", true) -- Insert spaces instead of tab
 opt_manager.add_option_buffer(0, "shiftwidth", 4) -- Affects <<, >>, and auto indent
 opt_manager.add_option_buffer(0, "smartindent", true) -- Automatically indent on a new line
 opt_manager.add_option_buffer(0, "softtabstop", 4) -- Number of spaces a <Tab> accounts for when editing
+opt_manager.add_option_buffer(0, "spelllang", "en_GB,de,es,cjk") -- Dictionarys to use for checking spelling
 opt_manager.add_option_buffer(0, "tabstop", 4) -- How many spaces a <Tab> in a file accounts for
 opt_manager.add_option_buffer(0, "undofile", true) -- Keep a file with the undo stack
 
