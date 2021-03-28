b7e0cdf767332fd7f21b7b81425c1df7ad2412be
b3nj4m1n <b3nj4m1n@gmx.net>
2021-03-28 15:21:14 +0200

    Polybar multi-monitor

--- files/polybar/polybar.sh
+++ files/polybar/polybar.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
-killall -q polybar
+# # Terminate already running bar instances
+# killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+# # Wait until the processes have been shut down
+# while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar bar &
+# # Launch polybar
+# polybar bar &
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload bar &
+    done
+else
+    polybar --reload bar &
+fi
