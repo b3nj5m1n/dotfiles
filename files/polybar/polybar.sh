11740e4b2633b0130ebf681938ac928667a4b770
b3nj4m1n <b3nj4m1n@gmx.net>
2021-03-28 17:44:00 +0200

    Dual Monitor

--- files/polybar/polybar.sh
+++ files/polybar/polybar.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
-# # Terminate already running bar instances
-# killall -q polybar
+# Terminate already running bar instances
+killall -q polybar
 
-# # Wait until the processes have been shut down
-# while pgrep -x polybar >/dev/null; do sleep 1; done
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# # Launch polybar
-# polybar bar &
+# Launch polybar
+polybar bar &
+polybar bar2 &
 
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload bar &
-    done
-else
-    polybar --reload bar &
-fi
+# if type "xrandr"; then
+#     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#         MONITOR=$m polybar --reload bar &
+#     done
+# else
+#     polybar --reload bar &
+# fi
