c8d9fc6842c7e1a26312777b327aae5727094a9c
b3nj4m1n <b3nj4m1n@gmx.net>
2021-06-15 19:19:46 +0200

    Add new wallpaper script

---
+++ scripts/wally.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURRENT_WALLPAPER="/usr/share/wallpapers/custom/moonRising.png"
+WALLPAPER="/home/b3nj4m1n/.config/wallpaper.png"
+
+# Copy the wallpaper to its appropriate location
+cp "$CURRENT_WALLPAPER" "$WALLPAPER"
+# Apply effect to wallpaper (Shift hue, i.e. change color)
+convert "$WALLPAPER" -modulate 100,100,$((0 + $RANDOM % 200)) "$WALLPAPER"
+# Set the new wallpaper
+feh --bg-scale "$WALLPAPER"
