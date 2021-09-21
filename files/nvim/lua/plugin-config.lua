82ebfb4cbfca1e41a17982e6efc6239a2d10d10e
b3nj4m1n <b3nj4m1n@gmx.net>
2021-09-21 23:02:40 +0200

    Add norg treesitter parser

--- files/nvim/lua/plugin-config.lua
+++ files/nvim/lua/plugin-config.lua
@@ -137,6 +137,17 @@ vim.api.nvim_exec('autocmd BufWritePre *.c lua vim.lsp.buf.formatting()', false)
 
 
 -- Treesitter
+
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_configs.norg = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg",
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "main"
+    },
+}
+
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
