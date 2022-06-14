fc66fd7dc15beb16091e055a9d2e3c857f5778e6
b3nj4m1n <b3nj4m1n@gmx.net>
2022-06-14 04:23:40 +0200

    Add ncpamixer padded script

---
+++ scripts/ncpamixer-padded.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+UUID="$(uuidgen)"
+
+tmux new -s "$UUID" -d -c "/bin/ncpamixer"
+tmux rename-window -t "$UUID" "PulseAudio Controll"
+tmux send-keys -t "$UUID" "ncpamixer && tmux kill-window -t \"$UUID\"" C-m
+tmux split-window -t "$UUID" -h ""
+tmux resize-pane -t "$UUID" -x 1
+tmux last-pane -t "$UUID"
+tmux split-window -t "$UUID" -hb ""
+tmux resize-pane -t "$UUID" -x 1
+tmux last-pane -t "$UUID"
+
+alacritty --working-directory "$HOME" --title "PulseAudio Controll" --command tmux attach -t "$UUID"
