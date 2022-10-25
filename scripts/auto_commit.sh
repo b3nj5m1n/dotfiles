b95fe5067027775ba1f7e09aa5b70b49d5bfc950
b3nj4m1n <b3nj4m1n@gmx.net>
2022-10-25 07:02:14 +0200

    Update auto_commit.sh

--- scripts/auto_commit.sh
+++ scripts/auto_commit.sh
@@ -12,7 +12,11 @@ EMAIL=""
 
 notify() {
     if [ "$SILENT" != true ]; then
-        XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "$1"
+        if command -v termux-setup-storage; then  
+            termux-notification -c "$1"
+        else
+            XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "$1"
+        fi
     fi
 }
 
@@ -29,8 +33,11 @@ EOM
 
 eval $(keychain --dir "$HOME/.cache/keychain" --eval --quiet "{{ ssh_key_name }}")
 
-cd "$REPO_DIR" && /usr/bin/git add --all || notify "Failed staging changes in $REPO_DIR"
-cd "$REPO_DIR" && /usr/bin/git -c user.name="$GIT_COMMITTER_NAME" -c user.email="$GIT_COMMITTER_EMAIL" commit --no-gpg-sign --author "$AUTHOR" -m "$MESSAGE" || notify "Failed committing changes in $REPO_DIR"
-cd "$REPO_DIR" && printf "$SSH_PW\n" | /usr/bin/git push || notify "Failed pushing changes in $REPO_DIR"
+cd "$REPO_DIR" && /usr/bin/git pull || notify "Failed pulling $REPO_DIR"
+if [[ $(git status --porcelain) ]]; then
+    cd "$REPO_DIR" && /usr/bin/git add --all || notify "Failed staging changes in $REPO_DIR"
+    cd "$REPO_DIR" && /usr/bin/git -c user.name="$GIT_COMMITTER_NAME" -c user.email="$GIT_COMMITTER_EMAIL" commit --no-gpg-sign --author "$AUTHOR" -m "$MESSAGE" || notify "Failed committing changes in $REPO_DIR"
+    cd "$REPO_DIR" && printf "$SSH_PW\n" | /usr/bin/git push || notify "Failed pushing changes in $REPO_DIR"
+fi
 
 notify "Done updating changes in $REPO_DIR"
