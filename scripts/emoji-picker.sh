376cd53753b7305b8e29963cd08631782d98bbe9
b3nj5m1n <b3nj4m1n@gmx.net>
2022-11-11 11:36:14 +0100

    Exit when running emoji-picker under wayland

--- scripts/emoji-picker.sh
+++ scripts/emoji-picker.sh
@@ -3,6 +3,11 @@
 # Opens a menu with a list of unicode emojis, the selection will be copied to clipboard or typed directly
 # Taken from here: https://github.com/LukeSmithxyz/voidrice
 
+if [[ $(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}') == "wayland" ]]; then
+    notify-send  "emoji picker" "Use bemoji" &
+    exit
+fi
+
 # Get user selection via rofi -dmenu from emoji file.
 # chosen=$(cut -d ';' -f1 ./assets/emoji.txt | rofi -dmenu -i -l 10 -fn "UbuntuMono Nerd Font Mono" -nf "#ff0066" | sed "s/ .*//")
 chosen=$(cut -d ';' -f1 /bin/scripts/assets/emoji.txt | rofi -dmenu -p "> " -fn "Twemoji" | sed "s/ .*//")
@@ -10,5 +15,4 @@ chosen=$(cut -d ';' -f1 /bin/scripts/assets/emoji.txt | rofi -dmenu -p "> " -fn 
 # Exit if none chosen.
 [ -z "$chosen" ] && exit
 
-echo "$chosen" | tr -d '\n' | xclip -selection clipboard
 notify-send  "emoji picker" "'$chosen' copied to clipboard." &
