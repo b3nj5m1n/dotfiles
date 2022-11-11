bd809670d4cfe8de9ab89314a2bed41ee581ba1e
b3nj5m1n <b3nj4m1n@gmx.net>
2022-11-11 08:17:29 +0100

    Migrate nixos config to Misterio77/nix-starter-configs template

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,49 +1,75 @@
-# flake.nix
 {
+  description = "Your new nix config";
+
   inputs = {
-    pkgs-stable.url = "github:NixOS/nixpkgs/release-22.05";
-    pkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-22.05";
+
     home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "pkgs-unstable";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
     hyprland = {
       url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "pkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    hardware.url = "github:nixos/nixos-hardware";
   };
 
-  outputs = { self, pkgs-stable, pkgs-unstable, home-manager, hyprland, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
-      system = "x86_64-linux";
-      overlays = [
-        (final: prev: {
-          stable = import inputs.pkgs-stable { system = final.system; };
-          unstable = import inputs.pkgs-unstable { system = final.system; };
-        }
-        )
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
       ];
-      pkgs = import pkgs-stable {
-        inherit system;
-        inherit overlays;
-      };
-      lib = pkgs-unstable.lib;
     in
-    {
+    rec {
+      # Your custom packages
+      # Acessible through 'nix build', 'nix shell', etc
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./pkgs { inherit pkgs; }
+      );
+      # Devshell for bootstrapping
+      # Acessible through 'nix develop' or 'nix-shell' (legacy)
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./shell.nix { inherit pkgs; }
+      );
+
+      # Your custom packages and modifications, exported as overlays
+      overlays = import ./overlays { inherit inputs; };
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
+      nixosModules = import ./modules/nixos;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
+      homeManagerModules = import ./modules/home-manager;
+
       nixosConfigurations = {
-        nixpad = lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit overlays; };
+        nixpad = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
           modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.b3nj4m1n = import ./home.nix;
-            }
-            hyprland.nixosModules.default
-            { programs.hyprland.enable = true; }
+            # > Our main nixos configuration file <
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        "b3nj4m1n@nixpad" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home-manager/home.nix
           ];
         };
       };
--- nix/nixos/hardware-configuration.nix
+++ nix/nixos/hardware-configuration.nix
@@ -37,4 +37,6 @@
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  nixpkgs.hostPlatform = "x86_64-linux";
 }
