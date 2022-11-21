67ad2895b2007c58ad25cdc0f3ab98490bce9bcf
b3nj4m1n <b3nj4m1n@gmx.net>
2022-11-21 12:00:16 +0100

    Add rust-tools.nvim

---
+++ files/nvim/ftplugin/rust.lua
@@ -0,0 +1,40 @@
+local opts = {
+    tools = {
+        runnables = {
+            use_telescope = true
+        },
+        executor = require("rust-tools.executors").termopen,
+        on_initialized = nil,
+        reload_workspace_from_cargo_toml = true,
+        inlay_hints = {
+            auto = true,
+            only_current_line = false,
+            show_parameter_hints = true,
+            parameter_hints_prefix = "  <- ",
+            other_hints_prefix = "  => ",
+            max_len_align = false,
+            max_len_align_padding = 1,
+            right_align = false,
+            right_align_padding = 7,
+            highlight = "Comment",
+        },
+        hover_actions = {
+            auto_focus = false,
+        },
+    },
+
+    server = {
+        standalone = true,
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    },
+
+    --[[ dap = {
+    adapter = {
+      type = "executable",
+      command = "lldb-vscode",
+      name = "rt_lldb",
+    },
+  }, ]]
+}
+
+require('rust-tools').setup(opts)
