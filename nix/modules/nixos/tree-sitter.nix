0cbcf9e3529ecf688e970fc380397cc4fbd774f0
b3nj5m1n <b3nj4m1n@gmx.net>
2023-03-09 04:09:59 +0100

    Fix tree-sitter-cli markdown

--- nix/modules/nixos/tree-sitter.nix
+++ nix/modules/nixos/tree-sitter.nix
@@ -2,19 +2,16 @@
 let
   system = pkgs.system;
 
-  pkgs_treesitter = import (builtins.fetchGit {
-    name = "tree-sitter-cli";
-    url = "https://github.com/mrene/nixpkgs";
-    ref = "refs/heads/tree-sitter-wrapper";
-    rev = "32abfde77f08a75587ec16b18b25f23eb9a640ff";
-   }) { inherit system; };
+  pkgs_treesitter = import (builtins.fetchTarball {
+    url = "https://github.com/mrene/nixpkgs/archive/refs/heads/tree-sitter-wrapper-test.tar.gz";
+    sha256 = "sha256:0a3lkl75cw58mma780wcbi9xx4v14m9in9n9scdva1mpalbi69yg";
+  }) { inherit system; };
+
 in {
   imports = [
   ];
 
   options = { };
-
-
 
   config = {
     environment.systemPackages = [
