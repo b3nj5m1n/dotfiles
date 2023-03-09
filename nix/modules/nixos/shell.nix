b68ef4ee26cd01096238c0e8d73219711c6b201e
b3nj5m1n <b3nj4m1n@gmx.net>
2023-03-09 02:09:52 +0100

    Add discordchatexporter-cli

--- nix/modules/nixos/shell.nix
+++ nix/modules/nixos/shell.nix
@@ -16,6 +16,7 @@
       cmatrix # A masterhacker essential
       difftastic # Better diff which understands syntax
       direnv # Automatically load env variables when entering directory
+      discordchatexporter-cli # Exactly what it says
       dotter # Dotfile management
       du-dust # More intuitive du
       duf # Display disk usage stats in a readable way
