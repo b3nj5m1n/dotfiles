4df0a007b7de6726ed402a95e1e0c219e5bf651f
b3nj5m1n <b3nj4m1n@gmx.net>
2023-03-31 13:12:20 +0200

    Add grimshot

--- nix/modules/nixos/sway.nix
+++ nix/modules/nixos/sway.nix
@@ -10,6 +10,7 @@
     environment.systemPackages = with pkgs; [
       swaylock-effects
       autotiling-rs
+      sway-contrib.grimshot
     ];
     programs.sway.enable = true;
     services.xserver.displayManager.defaultSession = "sway";
