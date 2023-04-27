faec04c526ea19ba21d6db450327672690e5a630
b3nj5m1n <b3nj4m1n@gmx.net>
2023-04-27 19:09:24 +0200

    xdg-desktop-portal still doesn't work ffs

--- nix/modules/nixos/wayland.nix
+++ nix/modules/nixos/wayland.nix
@@ -23,6 +23,13 @@
       wofi
     ];
     hardware.brillo.enable = true;
-    xdg.portal.extraPortals = with pkgs; [xdg-desktop-portal-hyprland];
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      # extraPortals = with pkgs; [
+      #   xdg-desktop-portal-wlr
+      #   xdg-desktop-portal-gtk
+      # ];
+    };
   };
 }
