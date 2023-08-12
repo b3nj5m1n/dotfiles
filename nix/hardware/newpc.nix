b8ac2d4cf338d91baa07d58d13a3e077d30b66f5
b3nj5m1n <b3nj4m1n@gmx.net>
2023-08-12 22:46:05 +0200

    Add swapfile

--- nix/hardware/newpc.nix
+++ nix/hardware/newpc.nix
@@ -82,7 +82,12 @@
   #   options = ["compress-force=zstd"];
   # };
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/swap/swapfile";
+      size = 24576;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
