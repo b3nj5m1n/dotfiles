0d090dee917d858d96dee6ca5757a9cdbdae6680
b3nj5m1n <b3nj4m1n@gmx.net>
2023-10-01 02:06:49 +0200

    Update beets config

--- scripts/downscale-music-collection.sh
+++ scripts/downscale-music-collection.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/bash
+#! /usr/bin/env nix-shell
+#! nix-shell -i dash -p dash findutils ffmpeg-full
 
 # This will loop over all files in the music dir, find files with a high bitrate
 # and convert them down to a more reasonable bitrate. Credit: https://askubuntu.com/a/536891
 
-music_dir="/mnt/Vault/music/"
+music_dir="/mnt/wolfpack/music/"
 
 cd "$music_dir" || exit
-/bin/find . -name '*.mp3' -exec sh -c 'ffprobe -show_streams -select_streams a:0 "$0" | grep -F "bit_rate=320000" && ffmpeg -y -i "$0" -c:a libmp3lame -b:a 128k /tmp/temp.mp3 && mv /tmp/temp.mp3 "$0"' '{}' \;
+find . -name '*.mp3' -exec sh -c 'ffprobe -show_streams -select_streams a:0 "$0" | grep -F "bit_rate=320000" && ffmpeg -y -i "$0" -c:a libmp3lame -b:a 128k /tmp/temp.mp3 && mv /tmp/temp.mp3 "$0"' '{}' \;
