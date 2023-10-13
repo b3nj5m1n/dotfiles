fd5e46c8e7ecaee0149bf7616672db51e96b6614
b3nj5m1n <b3nj4m1n@gmx.net>
2023-10-13 22:42:09 +0200

    Switch to wpa_supplicant

--- nix/modules/nixos/networking.nix
+++ nix/modules/nixos/networking.nix
@@ -9,14 +9,21 @@
       iwd
       iwgtk
       nmap
+      wpa_supplicant_gui
+    # networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
       wireguard-tools
+      openconnect # VPN client compatible with anyconnect protocol
     ];
-    networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-    networking.networkmanager.wifi.backend = "iwd";
+    # networking.networkmanager.wifi.backend = "iwd";
     networking.nftables.enable = true;
     networking.firewall = {
       enable = true;
       allowedTCPPorts = [22 30021];
     };
+    networking.wireless.userControlled.enable = true;
+    networking.wireless.enable = true;
+    networking.wireless.extraConfig = ''
+
+    '';
   };
 }
