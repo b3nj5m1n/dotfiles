b6d2ed39ae1aff673eb09f601913e508311e478c
b3nj5m1n <b3nj4m1n@gmx.net>
2023-11-28 11:11:17 +0100

    Add steam on thinkpad

--- nix/nixos/adelie.nix
+++ nix/nixos/adelie.nix
@@ -1,30 +1,32 @@
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
+{ inputs
+, outputs
+, lib
+, config
+, pkgs
+, ...
 }: {
-  imports = let
-    user = "b3nj4m1n";
-    args = {
-      inherit user;
-      inherit pkgs;
-    };
-  in [
-    (outputs.nixosModules args).base
-    (outputs.nixosModules args).shared-repos
-    (outputs.nixosModules args).terminal
-    (outputs.nixosModules args).sway
-    (outputs.nixosModules args).all-languages
-    # (outputs.nixosModules args).pandoc
-    (outputs.nixosModules args).aria2
-    (outputs.nixosModules args).battery-thing
-    (outputs.nixosModules args).pix2tex
+  imports =
+    let
+      user = "b3nj4m1n";
+      args = {
+        inherit user;
+        inherit pkgs;
+      };
+    in
+    [
+      (outputs.nixosModules args).base
+      (outputs.nixosModules args).shared-repos
+      (outputs.nixosModules args).terminal
+      (outputs.nixosModules args).sway
+      (outputs.nixosModules args).all-languages
+      # (outputs.nixosModules args).pandoc
+      (outputs.nixosModules args).aria2
+      (outputs.nixosModules args).battery-thing
+      (outputs.nixosModules args).pix2tex
+      (outputs.nixosModules args).steam
 
-    ../hardware/x270.nix
-  ];
+      ../hardware/x270.nix
+    ];
 
   nixpkgs = {
     overlays = [
@@ -34,6 +36,11 @@
     ];
     config = {
       allowUnfree = false;
+      allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "steam"
+        "steam-original"
+        "steam-run"
+      ];
       permittedInsecurePackages = [
         "python-2.7.18.6"
       ];
@@ -43,7 +50,7 @@
   nix = {
     # This will add each flake input as a registry
     # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
 
     # This will additionally add your inputs to the system's legacy channels
     # Making legacy nix commands consistent as well, awesome!
@@ -59,7 +66,7 @@
 
   networking.hostName = "adelie";
 
-  boot.supportedFilesystems = ["btrfs"];
+  boot.supportedFilesystems = [ "btrfs" ];
   hardware.enableRedistributableFirmware = true;
   boot.loader = {
     efi.canTouchEfiVariables = true;
@@ -77,16 +84,16 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = ["wheel" "video"];
+      extraGroups = [ "wheel" "video" ];
     };
   };
 
-    services.xserver.displayManager.lightdm.enable = true;
-    services.xserver.displayManager.lightdm = {
-        greeters = {
-            slick.enable = true;
-        };
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm = {
+    greeters = {
+      slick.enable = true;
     };
+  };
   services.xserver.desktopManager.plasma5.enable = true;
   programs.kdeconnect.enable = true;
 
