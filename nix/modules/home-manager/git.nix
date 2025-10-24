86c76e6bf520fcbebf042d9729fc507a12363271
b3nj5m1n <b3nj4m1n@gmx.net>
2025-10-24 17:42:18 +0200

    Formatting

--- nix/modules/home-manager/git.nix
+++ nix/modules/home-manager/git.nix
@@ -12,20 +12,20 @@
     programs.git = {
       enable = true;
       settings = {
-          alias = {
-            pr = "!f() { git fetch -fu \${2:-origin} refs/pull/\$1/head:pr/\$1 && git checkout pr/\$1; }; f"; # https://stackoverflow.com/a/14969986
-          };
-          user.email = "b3nj4m1n@gmx.net";
-          user.name = "b3nj5m1n";
-          init.defaultBranch = "main";
+        alias = {
+          pr = "!f() { git fetch -fu \${2:-origin} refs/pull/\$1/head:pr/\$1 && git checkout pr/\$1; }; f"; # https://stackoverflow.com/a/14969986
+        };
+        user.email = "b3nj4m1n@gmx.net";
+        user.name = "b3nj5m1n";
+        init.defaultBranch = "main";
       };
       signing = {
         signByDefault = true;
       };
     };
     programs.difftastic = {
-        enable = true;
-        git.enable = true;
+      enable = true;
+      git.enable = true;
     };
   };
 }
--- nix/nixos/emperor.nix
+++ nix/nixos/emperor.nix
@@ -53,7 +53,7 @@
         # "https://cache.nixos.org"
       ];
       trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
