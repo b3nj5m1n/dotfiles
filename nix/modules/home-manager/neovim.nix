47b477a3dc9748948e0a4af09dd3c8263efff6b4
b3nj5m1n <b3nj4m1n@gmx.net>
2026-05-26 16:55:24 +0200

    Make nvim have a config again

--- nix/modules/home-manager/neovim.nix
+++ nix/modules/home-manager/neovim.nix
@@ -12,5 +12,30 @@
     home.packages = with pkgs; [luajitPackages.luarocks];
     home.sessionVariables.EDITOR = "nvim";
     programs.neovim.enable = true;
+    programs.neovim.initLua = ''
+	-- vim.cmd [[packadd packer.nvim]]
+	vim.cmd [[packadd aniseed]]
+
+	--[[ require('packer').startup(function(use)
+	  -- Packer can manage itself
+	  use 'wbthomason/packer.nvim'
+	  use 'Olical/aniseed'
+	end) ]]
+
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+	  vim.fn.system({
+	    "git",
+	    "clone",
+	    "--filter=blob:none",
+	    "https://github.com/folke/lazy.nvim.git",
+	    "--branch=stable", -- latest stable release
+	    lazypath,
+	  })
+	end
+	vim.opt.rtp:prepend(lazypath)
+
+	require('aniseed.env').init()
+    '';
   };
 }
