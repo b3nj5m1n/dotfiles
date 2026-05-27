40928d4396c60774f2b0573f94b7d4a934a83dc9
b3nj5m1n <b3nj4m1n@gmx.net>
2026-05-27 09:16:48 +0200

    Formatting

--- nix/modules/home-manager/neovim.nix
+++ nix/modules/home-manager/neovim.nix
@@ -15,29 +15,29 @@
     programs.neovim.withRuby = true;
     programs.neovim.withPython3 = true;
     programs.neovim.initLua = ''
-	-- vim.cmd [[packadd packer.nvim]]
-	vim.cmd [[packadd aniseed]]
+      -- vim.cmd [[packadd packer.nvim]]
+      vim.cmd [[packadd aniseed]]
 
-	--[[ require('packer').startup(function(use)
-	  -- Packer can manage itself
-	  use 'wbthomason/packer.nvim'
-	  use 'Olical/aniseed'
-	end) ]]
+      --[[ require('packer').startup(function(use)
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
+        use 'Olical/aniseed'
+      end) ]]
 
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not vim.loop.fs_stat(lazypath) then
-	  vim.fn.system({
-	    "git",
-	    "clone",
-	    "--filter=blob:none",
-	    "https://github.com/folke/lazy.nvim.git",
-	    "--branch=stable", -- latest stable release
-	    lazypath,
-	  })
-	end
-	vim.opt.rtp:prepend(lazypath)
+      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+      if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+          "git",
+          "clone",
+          "--filter=blob:none",
+          "https://github.com/folke/lazy.nvim.git",
+          "--branch=stable", -- latest stable release
+          lazypath,
+        })
+      end
+      vim.opt.rtp:prepend(lazypath)
 
-	require('aniseed.env').init()
+      require('aniseed.env').init()
     '';
   };
 }
--- nix/modules/nixos/desktop.nix
+++ nix/modules/nixos/desktop.nix
@@ -56,14 +56,14 @@
 
     # TODO Use the same DM config on both systems
     services.displayManager.ly = {
-        enable = true;
-        settings = {
-            animation = "dur_file";
-            dur_file_path = "/usr/local/share/wallpaper/blackhole.dur";
-            asterisk = "0x2022";
-            vi_mode = true;
-            vi_default_mode = "insert";
-        };
+      enable = true;
+      settings = {
+        animation = "dur_file";
+        dur_file_path = "/usr/local/share/wallpaper/blackhole.dur";
+        asterisk = "0x2022";
+        vi_mode = true;
+        vi_default_mode = "insert";
+      };
     };
     /*
     services.xserver.displayManager.lightdm = {
